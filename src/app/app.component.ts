06e893348b51583522f94c13816d93a38185949d
Ajit Warrier <ajith.warrier2000@gmail.com>
2023-08-15 14:21:55 +0530

    Added Navbar to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'movies-app';
-
-  today = new Date();
-
-  imageURL = "HTTPS://DOTNETHOW.NET/IMAGES/MOVIES/MOVIE-1.JPEG";
-
-  price = 10;
-
-  sellingPrice = 15.50;
+  
 }
