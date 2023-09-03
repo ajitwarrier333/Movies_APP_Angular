35cbf7d447fda8e965d272975d32850a477a30c3
Ajit Warrier <ajith.warrier2000@gmail.com>
2023-09-03 21:57:22 +0530

    Iframe and Actors component added

---
+++ src/app/components/app-details-actors/app-details-actors.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-details-actors',
+  templateUrl: './app-details-actors.component.html',
+  styleUrls: ['./app-details-actors.component.css']
+})
+export class AppDetailsActorsComponent {
+
+}
